2b9a35e9e5a01f5e9238769e85dd0cfe21334bb8
naru0u <1912305@mwu.jp>
2025-04-03 22:12:25 +0900

    lesson3課題

---
+++ src/Review01.java
@@ -0,0 +1,16 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        // int Costが原価　 double taxinが税価格
+        int Cost = 1500;
+        int taxin = tax(1500);
+
+        System.out.println(Cost+"円の商品の税込価格は"+(Cost+taxin)+"円（消費税は"+taxin+"円）です。");
+ }
+
+    public static  int tax(int x) {
+        double result =(int) x*0.1;
+        return (int) result;
+    }
+}
